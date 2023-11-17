589556cd14dfb3805c0f0f407cb4734060ccf3af
Prashant-S29 <prashant.s2922@gmail.com>
2023-11-17 13:11:03 +0530

    tournament constant updatedt

--- tournament-constants/index.js
+++ tournament-constants/index.js
@@ -3,27 +3,7 @@
 import { erangle_card } from "@/assets"
 
 const Erangle = [
-    {
-        t_name: "Erangel 2.0 - Classic",
-        t_img: erangle_card,
-        t_id: "erangle",
-        status: "live",
-        t_fee: "₹120/-",
-        t_category: "team",
-        t_size: "Duo / Trio / Squad",
-        t_date: "17th-18th November, 2023",
-        t_time: "09:00 PM Onwards",
-        t_prize: "₹2100/-",
-        t_rule: [
-            "There will Point Table system for finalizing the winners.",
-            "There are 3 maps, Erangel | Sanhok | Miramar",
-            "It is a 2-4 player team tournamenent. You can participate as Duo, Trio or as a whole Squad.",
-            "No Emulator players are allowed.",
-            "Any use of unfair means such as aimbot, trigger bot, ESP etc are not allowed.",
-            "Organizers would not be held responsible for any connectivity issues on the participant's side.",
-            "Point Table is based on the BGIS leatest Rules. Kindly go through it once"
-        ]
-    },
+    
     {
         t_name: "Erangel - Classic",
         t_img: erangle_card,
